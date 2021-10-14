6f31b3146e190c5d251133e2d26c2885658fa681
Dorian Javá Brown <zeusofohio@gmail.com>
2021-10-14 00:44:03 -0400

    Removed Gem files and extraneous code

--- assets/global.js
+++ assets/global.js
@@ -1,3 +1,23 @@
+
+
+// Custom 
+let throttleTimer;
+ 
+const throttle = (callback, time) => {
+  if (throttleTimer) return;
+    throttleTimer = true;
+    setTimeout(() => {
+        callback();
+        throttleTimer = false;
+    }, time);
+}
+ 
+window.addEventListener("scroll", () => { 
+  throttle(handleScrollAnimation, 250);
+});
+
+// End Custom
+
 function getFocusableElements(container) {
   return Array.from(
     container.querySelectorAll(
